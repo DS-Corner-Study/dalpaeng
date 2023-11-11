264c0b43a2868a6727ad31ca73c229a2c64a511c
unknown <20220876@duksung.ac.kr>
2023-11-11 16:41:51 +0900

    [1111]prj1

--- week5/src/component/Body.js
+++ week5/src/component/Body.js
@@ -1,18 +1,18 @@
 function Body() {
-    function handleOnClick(e) {
-      console.log(e);
-      console.log(e.target.name);
-    }
-    return (
-      <div className="body">
-        <button name="A버튼" onClick={handleOnClick}>
-          A 버튼
-        </button>
-        <button name="B버튼" onClick={handleOnClick}>
-          B 버튼
-        </button>
-      </div>
-    );
+  function handleOnClick(e) {
+    console.log(e);
+    console.log(e.target.name);
   }
-  
-  export default Body;
+  return (
+    <div className="body">
+      <button name="A버튼" onClick={handleOnClick}>
+        A 버튼
+      </button>
+      <button name="B버튼" onClick={handleOnClick}>
+        B 버튼
+      </button>
+    </div>
+  );
+}
+
+export default Body;
